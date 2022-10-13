d34af2ce5ceaf03220a5dd2ed9e4aeef68340b96
Crafty-Raccoon <Kostoev-23@yandex.ru>
2022-10-13 19:36:10 +0300

    task1 compete

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number;
+number = int.Parse(Console.ReadLine());
+int temp = number;
+if (number > 9999 && number < 100000)
+{
+    int reverted = 0;
+    while (number > reverted) 
+    {
+        reverted = reverted * 10 + number % 10;
+        number /= 10;
+    }
+    if (number == reverted / 10) 
+    {
+        Console.WriteLine($"Число {temp} полиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Число {temp} не полиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Число не пятизначное");
+}
+
+Console.ReadKey();
